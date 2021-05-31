0dc9bc0fd429888ff64e3ce954fe92e429af1d8b
Omer Sabah <omerrj99@gmail.com>
2021-05-31 14:40:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
-const mondayWork = (activity="go to the office")=> 
-   `This Monday, I will ${activity}.`
+const mondayWork = (activity="go to the office")=> `This Monday, I will ${activity}.`
